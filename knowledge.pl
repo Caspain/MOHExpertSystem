cfbe7260f9d63cf69f2d244f49211cf1e1b9659d
remar_000 <remariorich@gmail.com>
2016-10-19 09:18:13 -0500

    finished with bmi calculations

--- knowledge.pl
+++ knowledge.pl
@@ -6,3 +6,15 @@ underweight([18.5]). % below
 normalweight([18.5,24.9]). % over and less than 24.9
 overweight([25,29.9]) . % 25 - 29.9	Overweight
 obese(30.0). % over 30
+
+
+% BMI Calculations
+printHeight(Height):- write("Height in meters =  "), nl,(format('~3f',[Height])).
+convertToM(CalcMeters):- CalcM is (CalcMeters / 100),printHeight(CalcM).
+convertToCm(CalcFeetInches):- CalcCm is (CalcFeetInches * 2.540),convertToM(CalcCm).
+
+calculate_height(Feet,Inches):- 
+ CalcFeet is (Feet * 12 ), CalcFeetInches is 
+ (CalcFeet + Inches), convertToCm(CalcFeetInches).
+ 
+ 
