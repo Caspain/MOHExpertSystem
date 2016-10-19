ca835a95bd02ec33a9cae51cdef37a550ecc27ff
remar_000 <remariorich@gmail.com>
2016-10-19 09:32:23 -0500

    finished adding bmi classifiers

--- knowledge.pl
+++ knowledge.pl
@@ -9,7 +9,7 @@ obese(30.0). % over 30
 
 
 % BMI Calculations
-printHeight(Height):- write("Height in meters =  "), nl,(format('~3f',[Height])).
+printHeight(Height):- write("Height in meters =  "), nl,(format('~3f',[Height])),classify_bmi(Height).
 convertToM(CalcMeters):- CalcM is (CalcMeters / 100),printHeight(CalcM).
 convertToCm(CalcFeetInches):- CalcCm is (CalcFeetInches * 2.540),convertToM(CalcCm).
 
@@ -17,4 +17,8 @@ calculate_height(Feet,Inches):-
  CalcFeet is (Feet * 12 ), CalcFeetInches is 
  (CalcFeet + Inches), convertToCm(CalcFeetInches).
  
- 
+classify_bmi(Bmi):-  (Bmi >= 30.0 -> Status = 'Obese'; Bmi < 18.5 -> 
+Status = 'UnderWeight'; Bmi >= 18.5 ,
+ Bmi =< 24.9 -> Status = 'NormalWeight';
+ Bmi >= 25 , Bmi =< 29.9 -> Status = 'OverWeight'
+),nl,write(Status).
