df64abf112bdb0bb27caa7c82bd4cfd3fcfdc9a5
remar_000 <remariorich@gmail.com>
2016-10-19 08:34:47 -0500

    added bmi contraints

--- knowledge.pl
+++ knowledge.pl
@@ -1,2 +1,8 @@
-/*knowledge base for moh*/
+% knowledge base for moh
 
+% BMI WEIGHT CATEGORIES
+
+underweight([18.5]). % below 
+normalweight([18.5,24.9]). % over and less than 24.9
+overweight([25,29.9]) . % 25 - 29.9	Overweight
+obese(30.0). % over 30
